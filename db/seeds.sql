14ccc3838d60732d1e8ed2466a45a154e9d17d9b
cpesar <77510555+cpesar@users.noreply.github.com>
2021-05-30 22:12:33 -0600

    created employees database

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (id, department_id)
+VALUES
+  (1, "Sales"),
+  (2, "Engineering"),
+  (3, "Finance"),
+  (4, "Legal");
+
+
+INSERT INTO roles (id, role_title, role_salary, department_id)
+VALUES
+
+
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //REQUIRED PACKAGES
+const generateServer = require('./server');
 const inquirer = require('inquirer');
 const fs = require ('fs');
 
@@ -9,8 +10,15 @@ inquirer.prompt ([
     type: "list",
     name: "options",
     message: "Please select from the following options (use arrows)",
-    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a      role", "Add an employee", "Update an employee role"]
-
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
   },
 
 ])
+
+
+// FUNCTION TO WRITE TO SERVER FILE
+.then (response => 
+  {
+    fs.writeFileSync('server.js')
+    console.log('Successfully wrote to server.js');
+})
